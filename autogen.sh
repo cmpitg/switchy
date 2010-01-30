bc217f39e694ec095a9855cca7c3b10a6cb3d1b0
nigel.tao.gnome@a151239e-a51d-0410-ae3d-21baef35e071 <nigel.tao.gnome@a151239e-a51d-0410-ae3d-21baef35e071>
2010-01-30 23:57:08 +0000

    Fix typo in autogen.sh. This fixes issue issue 37.
    --HG--
    extra : convert_revision : svn%3Aa151239e-a51d-0410-ae3d-21baef35e071/trunk%4038

--- autogen.sh
+++ autogen.sh
@@ -4,7 +4,7 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="superswticher"
+PKG_NAME="superswitcher"
 
 (test -f $srcdir/configure.in) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
